9755fa1eadc81a51f474921a6d661a5077c0c391
unknown <brittanycturner@gmail.com>
2018-02-14 08:13:00 -0700

    replace the _ with a letter

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,18 +5,19 @@ var wordChoices = ["beagle", "poodle", "dachshund", "corgi"];
 var currentWord = wordChoices[Math.floor(Math.random() * wordChoices.length)];
 
 function showCurrentWord() { 
-    var wordChoiceDiv = document.getElementById("wordChoices");
-    wordChoiceDiv.innerHTML = currentWord; //This is showing the actual word, remove this later
+    var wordChoiceDiv = document.getElementById("wordChoices"); 
+    wordChoiceDiv.innerHTML = currentWord; // This is showing the actual word, remove this later
     for (var i = 0; i < currentWord.length; i++) {        // for every letter in the "current word", type a "_"
         var newSpan = document.createElement("span");
         newSpan.innerHTML= "_ ";
+        newSpan.setAttribute("id","letter-"+i);
         wordChoiceDiv.appendChild(newSpan);  
-      }
+      } 
 }
 
 showCurrentWord();
  
-
+// create an array based on the length of the word filled with underscores, and then replacing the _ in the array
 
 // user gueses a letter, if letter matches letter in currentWord then replace _ with letter, else, type letter in "letters used" & remove 1 from guess
 // Match for loop in other function ("retype the _ everytime kind of"), if B=charater 1 type B, else, type _
@@ -29,26 +30,29 @@ var userGuess = [];
 
 document.onkeyup = function(event) {
     userGuess.push(event.key); // Add the key that was typed to the userGuess array
+    
     if (userGuess[0] === currentWord.charAt(0)) { // Need to make this a for loop, currently 
-    alert(userGuess[0]);
+        var CurrentBlank = document.getElementById("letter-0");
+        CurrentBlank.textContent = (userGuess[0]);  // if UserGuess[i] === Charat(), change "element1" to UserGuess[i]
+        //May need to assign specific ids instead of adding spans
     } 
     else {
     alert("you suck");
     }
 }
 
-
+console.log
 
     
     
     //for (var j = 0; j < userGuess.length; j++) {        // for every letter in the "current word", type a "_"
         //var newSpan = document.createElement("span");
        // if (userGuess[j] = currentWord.charAt(0)) {
-            console.log(userGuess[j]);
+            //console.log(userGuess[j]);
             // newSpan.innerHTML= "userGuess[j]";
             // wordChoiceDiv.appendChild(newSpan);
      //   }  else {
-            console.log("nope!");
+           // console.log("nope!");
             // newSpan.innerHTML= "_ ";
             // wordChoiceDiv.appendChild(newSpan);  
        // } 
